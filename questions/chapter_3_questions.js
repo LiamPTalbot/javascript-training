ae8c7752f51e746b1fe2d67f45aa812eda199af2
LiamPTalbot <liam.p.talbot@gmail.com>
2024-09-16 13:56:34 +0100

    end of session

---
+++ questions/chapter_3_questions.js
@@ -0,0 +1,58 @@
+// 1. Array Fitlering - write a function that filters out all the even numbers from an array of integers.
+// start with pseudo-code
+
+// set up an array of integers
+// define even numbers
+// write a function that filters out even numbers
+// write a function that checks if an integer is even
+// loop that function over the array
+// pass the array in to the function
+// console.log the result
+
+let array_of_integers = [0,1,2,3,4,5,6,7,8,9,10]
+
+function remove_evens (N) {
+    let array_of_odds = []
+    for (let i = 0; i < N.length; i++) {
+        if (i % 2 !== 0) {
+            array_of_odds.push(i)
+        }
+    }
+    return array_of_odds
+}
+
+console.log(remove_evens(array_of_integers))
+
+// 2. Object manipulation: given an array of objects representing people with names and ages, write a function to find the person with the highest age.
+// start with pseudo-code
+// build an array of objects representing people with names and ages
+// build a function to sort in age order
+// 
+// console.log the [o] object in the array
+
+let array_of_people = [
+    {
+        name: 'Liam',
+        age: 35
+    },
+    {
+        name: 'Sheilla',
+        age: 41
+    },
+    {
+        name: 'Nydia',
+        age: 75
+    }
+]
+
+arr = [75, 41, 35]
+
+function age_sorter(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] <= arr[i+1]) {
+            continue
+        } else {
+            
+        }
+    }
+}
