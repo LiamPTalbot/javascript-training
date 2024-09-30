a17827632c965546681540ae399025c54ff334e4
LiamPTalbot <liam.p.talbot@gmail.com>
2024-09-30 14:45:19 +0100

    finished chapter 6 - Async Programming

--- questions/chapter_6_questions.js
+++ questions/chapter_6_questions.js
@@ -40,3 +40,17 @@ async function fetch_user_data() {
 
 // 4. Demonstrate how to make a POST request to 'https://api.example.com/users' to create a new user with JSON data { name: "John", age: 30 } using async/await. Ensure you set the appropriate headers and catch any errors.
 
+async function post_user_data() {
+    try {
+        const options = {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({name: Liam})
+            }
+        const res = await fetch ('https://the-one-api.dev/v2/books', options ) //res is response from the endpoint URL
+    } catch(err) {
+        console.log('Error fetching data')
+    }
+}
